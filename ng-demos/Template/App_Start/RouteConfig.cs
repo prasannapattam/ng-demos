b2c63900b8a597c5bf9d1bc0cbb474df79dc1a3d
prasannapattam <pprasannak@hotmail.com>
2014-07-23 20:14:09 +0530

    Changed the default routing

--- ng-demos/Template/App_Start/RouteConfig.cs
+++ ng-demos/Template/App_Start/RouteConfig.cs
@@ -14,9 +14,9 @@ namespace AngularKendoBootstrap
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                "default",
+                "{*url}",
+                new { controller = "Home", action = "Index" }
             );
         }
     }
