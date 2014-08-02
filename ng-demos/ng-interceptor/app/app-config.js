ff75878259ab198c81edda60d3d67b111f2c3ea8
prasannapattam <pprasannak@hotmail.com>
2014-08-02 19:08:55 +0530

    Added a sample interceptor

---
+++ ng-demos/ng-interceptor/app/app-config.js
@@ -0,0 +1,27 @@
+﻿'use strict';
+
+angular.module('appinterceptors').config(appConfig);
+routeConfig.$inject = ['$routeProvider', '$locationProvider', '$httpProvider'];
+
+function appConfig($routeProvider, $locationProvider, $provide, $httpProvider) {
+
+    //routing
+    $routeProvider
+      .when("/home", {
+        templateUrl: "/app/home/home.html",
+        controller: "home",
+        controllerAs: "vm"
+      })
+      .when("/contact", {
+        templateUrl: "/app/contact/contact.html",
+        controller: "contact",
+        controllerAs: "vm"
+      })
+      .otherwise({ redirectTo: "/home" });
+    $locationProvider.html5Mode(true);
+
+    // Add the interceptor to the $httpProvider.
+    $httpProvider.interceptors.push('WebAPIInterceptor');
+
+}
+
---
+++ ng-demos/ng-interceptor/app/app-interceptor.js
@@ -0,0 +1,46 @@
+﻿angular.module('appinterceptors').factory('WebAPIInterceptor', WebAPIInterceptor);
+WebAPIInterceptor.$inject = ['$q'];
+
+function WebAPIInterceptor($q) {
+
+    return {
+        request: request,
+        requestError: requestError,
+        response: response,
+        responseError: responseError
+    };
+
+    //request success
+    function request(config) {
+        console.log(config); 
+
+        // Return the config or promise.
+        return config || $q.when(config);
+    }
+
+    //request error
+    function requestError(rejection) {
+        console.log(rejection); 
+
+        // Return the promise rejection.
+        return $q.reject(rejection);
+    }
+
+    // response success
+    function response(response) {
+        console.log(response);
+
+        // Return the response or promise.
+        return response || $q.when(response);
+    }
+
+    //response Error
+    function responseError(rejection) {
+        console.log(rejection); // Contains the data about the error.
+
+        // Return the promise rejection.
+        return $q.reject(rejection);
+    }
+}
+
+
